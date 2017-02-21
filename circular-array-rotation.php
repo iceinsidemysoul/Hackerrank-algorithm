c1c11264c6118bd2e932cdc4a026f5326f270ac5
iceinsidemysoul <sompong.paramon@gmail.com>
2017-02-21 16:21:18 +0700

    Update circular-array-rotation.php

--- circular-array-rotation.php
+++ circular-array-rotation.php
@@ -5,9 +5,15 @@ fscanf($handle,"%d %d %d",$n,$k,$q);
 $a_temp = fgets($handle);
 $a = explode(" ",$a_temp);
 array_walk($a,'intval');
-$k=$k%$n;
+$k%=$n;
+$cd=$a;
+for($i=0;$i<$n;$i++){
+        $temp=($i+$n-$k)%$n;
+        $a[$i]=(int)$cd[$temp];
+}
 for($a0 = 0; $a0 < $q; $a0++){
     fscanf($handle,"%d",$m);
+    echo $a[$m]."\n";
 }
 
 ?>
